cc52277100e2edca7d8062ca0561da6af700a56d
@PauloGandolfi <paulo_gandolfi@hotmail.com>
2023-07-28 23:25:35 -0300

    Uptade-Tickets

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
-function addRedBorder(id){
-    element = document.querySelector("#" + id);
-    element.style.border = "5px solid red";
-}
+function highlightCard(selector){
 
-addRedBorder("sexta");
+    var element = document.getElementById(selector);
+
+    element.classList.toggle("card-highlight");
+}
+function checkKeyboardCode(){
+    document.addEventListener('keydown', (dnc) => {
+        var name = dnc.key;
+        var code = dnc.code;
+        alert(`Tecla Pressionada ${name} \r\n code : ${code}`);
+
+    }, false);
+}
+function addKeyboardEventListeners(){
+    document.addEventListener('keydown' , (dnc) =>{
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = dnc.code;
+        if(code == 'Digit1'){
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+        var code = dnc.code;
+        if(code == 'Digit2'){
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+        var code = dnc.code;
+        if(code == 'Digit3'){
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+        var code = dnc.code;
+        if(code == 'Digit4'){
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+    }, false);
+}
+addKeyboardEventListeners();
